2a33e66c2cb6a769461a19857e616ee88e8b866e
BugHunter-X <askzenai@gmail.com>
2025-02-15 17:10:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+function foo(x) {
+  if (x === null) {
+    return 0;
+  } else if (x < 0) {
+    return -1;
+  } else if (isNaN(x)) {
+    return -2; // Or throw an error: throw new Error("Invalid input: NaN");
+  } else {
+    return x + 1; 
+  }
+}
+
+console.log(foo(null)); // Output: 0
+console.log(foo(-1));  // Output: -1
+console.log(foo(5));   // Output: 6
+console.log(foo(NaN)); // Output: -2
